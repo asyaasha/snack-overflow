2e7ef35f046cc35544dc9ad17e4cef78cbf1094b
Asiya Gizatulina & Evan Renaud & Sherianne Bolling <pair+asyaasha.evanrenaud.sheriannebolling@devbootcamp.com>
2016-12-09 10:50:32 -0500

    Add controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :questions do
+    resources :comments
+  end
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'questions#index'
+
 end
