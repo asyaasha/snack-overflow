056f23d9b22543aa74fd6b5e0dd629b1e164052d
Asiya Gizatulina & Evan Renaud & Sherianne Bolling <pair+asyaasha.evanrenaud.sheriannebolling@devbootcamp.com>
2016-12-09 15:09:47 -0500

    Start adding react functionality

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,37 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function Question(props) {
+  return(
+    );
+}
+class QuestionView extends React.Component {
+  }
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      questions:[],
+    }
+  }
+
+  fetchQuestions() {
+    $.ajax({
+      url:'/questions/index',
+      method:'get'
+    }).done(response => {
+      console.log(response);
+      this.setState({
+        questions:response
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchQuestions();
+  }
+
+  render() {
+    return(
+      <QuestionView questions={this.state.questions} />
+      );
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :answers
   end
 
+  resources :comments
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root 'questions#index'
