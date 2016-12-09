fe6bf917daccd986f3263e1e910c8ab85349f523
Asiya Gizatulina & Evan Renaud & Sherianne Bolling <pair+asyaasha.evanrenaud.sheriannebolling@devbootcamp.com>
2016-12-09 11:30:13 -0500

    Add views for questions and materalize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :authorized?
 
+  def logged_in?
+    !!current_user
+  end
 
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 
+  def authorized?(resource)
+    current_user == resource.user
+  end
+
+  def require_login
+    redirect_to new_session_path unless logged_in?
+  end
 end
